22de0a650b0a1701e0a0d2889a3de9dbbacc4f5f
maleye rane <maleyeranendoye@Mydebian.lan>
2023-09-21 16:07:58 +0200

    database connexion

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
+<?php
+?>
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Super Reminder</title>
+    <title>Document</title>
 </head>
 <body>
-    <div>this is the index</div>
-    
+    <div>ici notre router</div>
 </body>
 </html>
