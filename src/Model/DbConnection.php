9cc9d2b85da5b70f8d7fbd87766025b082972ffb
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-09-24 10:55:04 +0200

    Model database connection

---
+++ src/Model/DbConnection.php
@@ -0,0 +1,25 @@
+<?php
+namespace Model\DbConnection;
+ class DbConnection 
+{
+private static $servername = 'localhost';
+private static $username_b = 'root';
+private static $password_b = '';
+private static $database = 'superReminder';
+private static ?\PDO $_db = null;
+
+public static function getDb()
+{
+    if (!self::$_db) {
+        try {
+            self::$_db = new \PDO('mysql:dbname=' . self::$database . ';host=' . self::$servername . ';charset=utf8mb4', self::$username_b, self::$password_b);
+
+            self::$_db->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+        } catch (\PDOException $e) {
+            header("HTTP/1.1 403 Acces refused to the database");
+            die();
+        }
+    }
+    return self::$_db;
+}
+}
