518465d1bf65fc91a8d9d6cfcf854282ad7c08b5
maleye rane <maleyeranendoye@Mydebian.lan>
2023-09-26 16:16:49 +0200

    front home page

---
+++ View/homePage.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Home</title>
+</head>
+<body>
+     <section class="section">
+                <article class="left-aside">
+                        <div class="paraf-4">
+                               <div class="word-4">whath</div>
+                               <div>to</div>
+                               <div>do ?</div>
+
+                        </div>
+                        <div class="paraf-5">
+                                <div>organization</div>
+                        </div>
+                        <div class="paraf-6">
+                               <div class="sum-task">
+                                    <p>appointement 24</p>
+                                    <p>homework 12</p>
+                                    <p>learning time 15:20</p>
+                               </div>
+                               <div class="roller"></div>
+                        </div>
+
+                        <div class="paraf-7">
+                            <a class="signup" href="">sign up</a>
+                            <i class="fa-regular fa-heart"></i>
+                            <a class="addtasks" href="">add tasks</a>
+                           
+                        </div>
+                </article>
+                <article class="right-aside">
+                            <div class="paraf-1">create tasks</div>
+                            <div class="paraf-2">
+                                <p class="word-1">withe</p>
+                                <p class="word-2">to </p>
+                                <p class="word-3">do </p>
+                                <p class="word-4">list</p>
+                            </div>
+                                <div class="paraf-3">
+                                the new app creator of <br>
+                                sheet  task to remind at <br>
+                                time to do all your works <br>
+                                </div>
+                </article>
+     </section>
+</body>
+</html>
