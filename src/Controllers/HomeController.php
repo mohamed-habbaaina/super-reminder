3ab164c58975c9ced70c54d3b587a703f8a2e748
maleye rane <maleyeranendoye@Mydebian.lan>
2023-09-28 18:09:02 +0200

    composer init

---
+++ src/Controllers/HomeController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Controllers;
+
+class HomeController{
+
+    public function showHome($user = "maleye"){
+        echo 'maleye';
+        // require __DIR__ . '/../Views/homePage.php';
+    }
+}
