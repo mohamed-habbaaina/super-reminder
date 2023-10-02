fe101364e906818577cfd472cb9d51a464dbb058
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-10-02 16:22:48 +0200

    view html task, input checkBox

---
+++ View/tasks.php
@@ -0,0 +1,20 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script defer src="./js/handleTasks.js"></script>
+    <title>Super Reminder | Tasks</title>
+</head>
+<body>
+    <h1>Super Reminder</h1>
+
+    <input type="text" id="newList" placeholder="New Task ...">
+    <button id="addList">Add</button>
+
+
+    <div id="root"></div>
+</body>
+</html>
