9fd6e3ad04b5d8adf49a566acca5c3148d99eb9f
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-10-03 17:20:01 +0200

    JS, handle tasks

--- View/js/handleTasks.js
+++ View/js/handleTasks.js
@@ -1,21 +1,27 @@
 window.addEventListener('DOMContentLoaded', async () => {
 
+    // variables
     const display = document.getElementById('root');
-
     const newListInput = document.getElementById('newList');
     const addListButton = document.getElementById('addList');
+    
+    // event to add tasks (List table)
+    addListButton.addEventListener('click', addList)
 
-    // Écoutez le clic sur le bouton "Ajouter"
-    addListButton.addEventListener('click', async (e) => {
+    async function addList(e){
         e.preventDefault();
         const listText = newListInput.value.trim();
+        
+        if(listText.length > 1){
 
-        let insertList = await fetch('./../src/Controller/ControllerHandleTasks.php?listText=' + listText)
-        newListInput.value = '';
-        window.location.reload();
+            let insertList = await fetch('./../src/Controller/ControllerHandleTasks.php?listText=' + listText)
+            newListInput.value = '';
+            window.location.reload();
+        }
 
-    });
-
+    }
+    
+    // creating the elements HTML
     const response = await fetch('../src/Controller/ControllerHandleTasks.php');
     if(response.ok){
 
@@ -24,31 +30,62 @@ window.addEventListener('DOMContentLoaded', async () => {
         
         data.forEach(item => {
             
-            let div = document.createElement('div');
-            // let button = document.createElement('button');
+            // creating element dom
+            const divInProgress = document.createElement('div'),
+                  divDone = document.createElement('div'),
+                  checkbox = document.createElement('input'),
+                  taskLabel = document.createElement('label'),
+                  button = document.createElement('button');
 
-            const checkbox = document.createElement('input');
             checkbox.type = 'checkbox';
-            const taskLabel = document.createElement('label');
+            button.textContent = ' X '
     
+            // handle label
             taskLabel.innerHTML = item.title;
-            div.id = item.id_list;
+
+            // if status === 'fait' add class 'done'
+            if(item.status === 'fait'){
+                taskLabel.classList.add('done');
+                divDone.append(taskLabel, button);
+                divDone.id = item.id_list
+            } else {
+                divInProgress.append(taskLabel, checkbox, button);
+                divInProgress.id = item.id_list
+            }
             
-            div.append(taskLabel, checkbox);
+            // event to done task (change status)
+            checkbox.addEventListener('click', doneList);
             
-            checkbox.addEventListener('click', async (e)=>{
+            async function doneList(e){
+
+                e.preventDefault();
                 
-                console.log('ok checkBox');
+                let updatStatus = await fetch('./../src/Controller/ControllerHandleTasks.php?status=' + item.id_list)
+                window.location.reload();
+        
                 e.stopPropagation();
 
-            })
-            display.appendChild(div);
-            div.addEventListener('click', (e)=>{
+            }
+
+            // Add divs into the dom
+            display.append(divInProgress, divDone);
+
+            // Event delete list
+            button.addEventListener('click', deletList)
+            
+            async function deletList(e){
+                let deleteList = await fetch('./../src/Controller/ControllerHandleTasks.php?delete=' + item.id_list)
+                window.location.reload();
+                e.stopPropagation();
+
+            } 
+
+            // todo ceation tasks List event
+            divInProgress.addEventListener('click', (e)=>{
+
                 console.log('ok DIV');
                 e.stopPropagation();
             })
-            })
-        }
-
-
-    });
+        })
+    }
+});
