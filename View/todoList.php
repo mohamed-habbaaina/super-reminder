fe778df73c67b910e4b53588468decddc3da8526
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-10-03 17:20:38 +0200

    View todo list

--- View/todoList.php
+++ View/todoList.php
@@ -1,4 +1,9 @@
 <?php
+// session_start();
+// require_once('./../src/Model/ModelHandleTasks.php');
+// $test = new Model\ModelHandleTasks\ModelHandleTasks();
+// var_dump($test->getIdUser($_SESSION['email']));
+// var_dump($_SESSION);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -6,6 +11,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script defer src="./js/handleTasks.js"></script>
+    <link rel="stylesheet" href="./css/style.css">
     <title>Super Reminder | Tasks</title>
 </head>
 <body>
