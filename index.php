04b61703f2921d9592e4786adcbb15e302e70906
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-10-08 06:52:10 +0200

    change todoList page to index

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,10 +8,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <link rel="stylesheet" href="View/css/style.css">
+    <link rel="stylesheet" href="./View/css/style.css">
+    <script defer src="./View/js/handleTasks.js"></script>
+    <title>Super Reminder | Tasks</title>
 </head>
 <body>
+<header>
+        <?php
+            if(isset($_SESSION['autoris']) && $_SESSION['autoris'] === 'ok'){
+        ?>
+        <nav>
+            <ul>
+                <li><a href="./src/Controller/ControllerDeconnect.php">deconnetion</a></li>
+            </ul>
+        </nav>
+    </header>
      <section class="section">
+
                 <aside class="left-aside">
                         <div class="paraf-4">
                                <div class="word-4">whath</div>
@@ -16,39 +32,51 @@
                                <div>do ?</div>
 
                         </div>
-                        <div class="paraf-5">
-                                <div>organization</div>
+                        <div class="startline">
+                                <div>start to creat tasks</div>
                         </div>
-                        <div class="paraf-6">
-                               <div class="sum-task">
-                                    <p>appointement 24</p>
-                                    <p>homework 12</p>
-                                    <p>learning time 15:20</p>
-                               </div>
-                               <div class="roller"></div>
+                      
+                        <div class="arrea">
+                                <input type="text"  id="newList" placeholder="new Task ..." class="list-box"></input>
+                                <button id="addList" class="adding">Add</button>
                         </div>
-
-                        <div class="paraf-7">
-                            <a class="signup"  href="/B2/super-reminder/View/register.php">sign up</a>
-                            <i class="fa-regular fa-heart"></i>
-                            <a  class="signin" href="/B2/super-reminder/View/connection.php">sign in</a>
-                           
-                        </div>
+                       
                 </aside>
                 <aside class="right-aside">
-                            <div class="paraf-1">create tasks</div>
-                            <div class="paraf-2">
-                                <p class="word-1">withe</p>
+                            <div class="paraf-8">
                                 <p class="word-2">to </p>
                                 <p class="word-3">do </p>
                                 <p class="word-4">list</p>
                             </div>
-                                <div class="paraf-3">
-                                the new app creator of <br>
-                                sheet  task to remind at <br>
-                                time to do all your works <br>
-                                </div>
-                </aside>
-     </section>
+                             <div class="list-container">
+                               <ul>
+                                    <li>
+                                            <h2 id="root">Tasks :</h2>
+                                    </li>
+                                   
+                               </ul>
+                               
+                            </div>
+                        </aside>
+    </section>
+    <?php } else{ ;?>
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="./View/connection.php">connection</a></li>
+                            <li><a href="./View/register.php">register</a></li>
+                        </ul>
+                    </nav>
+                </header>
+
+                <div id="div_H_connect">
+                    <h1>Super Reminder</h1>
+                    <p>Please <a href="./View/connection.php">log in</a> ...</p>
+                </div>
+            <?php }?>
+
+<?php require_once('./View/include/footer.php'); ?>
+
+
 </body>
 </html>
